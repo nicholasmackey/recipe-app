ebd1945142e07ed613fe06f632029ecc6ccd9ed5
Nicholas Mackey <59402742+nikmackey@users.noreply.github.com>
2021-01-29 20:25:22 -0600

    feat: displays data upon search

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-const baseEndPoint = 'http://www.recipepuppy.com/api';
+const baseEndpoint = 'http://www.recipepuppy.com/api';
+const proxy = `https://cors-anywhere.herokuapp.com/`;
+const form = document.querySelector('form.search');
+const recipesGrid = document.querySelector('.recipes');
 
 async function fetchRecipes(query) {
-    const res = await fetch(`https://cors-anywhere.herokuapp.com/${baseEndPoint}?=${query}`)
-    const data = await res.json();
+  const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
+  const data = await res.json();
+  return data;
 }
 
-fetchRecipes();
+async function handleSubmit(event) {
+  event.preventDefault();
+  const el = event.currentTarget;
+  fetchAndDisplay(form.query.value);
+}
+
+async function fetchAndDisplay(query) {
+  form.submit.disabled = true;
+  const recipes = await fetchRecipes(query);
+  form.submit.disabled = false;
+  displayRecipes(recipes.results);
+}
+
+function displayRecipes(recipes) {
+  const html = recipes.map(
+    recipe => `<div class="recipe">
+      <h2>${recipe.title}</h2>
+      <p>${recipe.ingredients}</p>
+      <a href="${recipe.href}">View Recipe →</a>
+    </div>`
+  );
+  recipesGrid.innerHTML = html.join('');
+}
+
+form.addEventListener('submit', handleSubmit);
+fetchAndDisplay('pizza');
