485bf1b2dd80a8e44caa142df8975bab805e44e0
Nicholas Mackey <59402742+nikmackey@users.noreply.github.com>
2021-01-29 19:53:39 -0600

    configures scripts and works around CORS policy

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-
 const baseEndPoint = 'http://www.recipepuppy.com/api';
+
+async function fetchRecipes(query) {
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/${baseEndPoint}?=${query}`)
+    const data = await res.json();
+}
+
+fetchRecipes();
